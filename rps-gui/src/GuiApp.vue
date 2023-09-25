f022454e6c028b241dffc2768afd83bf21b2737c
alpah <dev@alpah.ru>
2023-09-25 22:47:41 +0200

    vite: added vitest

---
+++ rps-gui/src/GuiApp.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <GameInstructions />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import GameInstructions from './components/GameInstructions.vue';
+    function some() {
+        console.log('--- thing');
+    }
+</script>
+
+<style>
+    #app {
+    }
+</style>
